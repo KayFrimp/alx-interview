56ab9a71fba4e7eb4c45ed8c3d8b2806e6ff1fec
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-11 20:57:43 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,14 @@
+#!/usr/bin/python3
+"""canUnlockAll Module"""
+
+
 def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened
+       boxes is a list of lists with keys that can
+       open boxes
+
+       Returns:
+           True if all boxes can be unlocked else False"""
     n = len(boxes)
     unlocked_boxes = {0}
 
