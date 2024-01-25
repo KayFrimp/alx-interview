316fac8551733148c5fc59c0135ef7af0ad0db59
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-25 20:39:25 +0000

    operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,12 +3,9 @@
 
 
 def minOperations(n):
-    """Kep track of and return the number of operations performed"""
+    """Keep track of and return the number of operations performed"""
     if n <= 1:
         return 0
-
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
 
     operations = 0
     divisor = 2
