c608dad05e91dabfbcfc76b4eae39b62f818aa09
Kwabena A  Frimpong <kafrimpong23@gmail.com>
2024-02-03 22:28:28 +0000

    utf8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,15 +11,15 @@ def validUTF8(data):
         if not (0 <= data[i] <= 255):
             return False
         leading_bits = bin(data[i])[2:].zfill(8)[:4]
-        if leading_bits == '110':
-            if i + 1 >= len(data) or bin(data[i + 1])[2:3] != '10':
-                return False
-            i += 2
-        elif leading_bits == '1110':
-            if i + 2 >= len(data) or any(bin(byte)[2:3] != '10'
+        if leading_bits == '1110':
+            if i + 2 >= len(data) or any(byte < 128 or byte > 191
                                          for byte in data[i + 1:i + 3]):
                 return False
             i += 3
+        elif leading_bits == '110':
+            if i + 1 >= len(data) or data[i + 1] < 128 or data[i + 1] > 191:
+                return False
+            i += 2
         elif leading_bits == '10':
             return False  # Unexpected leading byte
         else:
