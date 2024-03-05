508b32da58fa420301d0c909aa0c6e3118246d35
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-03-05 21:38:17 +0000

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,11 +8,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
+    sorted_coins = sorted(coins, reverse=True)
+    remaining_total = total
+    coins_used = 0
 
-    for coin in coins:
-        for x in range(coin, total + 1):
-            min_coins[x] = min(min_coins[x], min_coins[x - coin] + 1)
+    for coin in sorted_coins:
+        while remaining_total >= coin:
+            remaining_total -= coin
+            coins_used += 1
 
-    return -1 if min_coins[total] == float('inf') else min_coins[total]
+    if remaining_total > 0:
+        return -1
+
+    return coins_used
